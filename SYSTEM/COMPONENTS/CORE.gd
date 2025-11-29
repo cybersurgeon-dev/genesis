897e7fe77b9c4d670f3c7599d695da6d86a05705
cybersurgeon <cyb3rsxrge0n@bk.ru>
2025-11-29 03:35:16 +0300

    :star: feat: add VDPFX support, Per-Line offset effect :truck: update: move CHECKERBOARD_BG to VDP_TESTING :lollipop: example: add VDP_TESTING scene :lipstick: update: change default palette generation color :wrench: update: change boot scene to VDP_TESTING

--- SYSTEM/COMPONENTS/CORE.gd
+++ SYSTEM/COMPONENTS/CORE.gd
@@ -2,4 +2,4 @@ extends Node
 
 func _ready():
 	VDP.SET_MODE(50,28)
-	get_tree().change_scene_to_file("res://SCENES/CHECKERBOARD_BG/CHECKERBOARD_BG.tscn")
+	get_tree().change_scene_to_file("res://SCENES/VDP_TESTING/CHECKERBOARD_BG.tscn")
